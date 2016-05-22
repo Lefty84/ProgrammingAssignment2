893685d14ace2c8a0d0e9c091f81b22852b7ed64
Lefty84 <Lefty84@users.noreply.github.com>
2016-05-22 12:40:31 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Matrix inversion is a computation-intensive and there are benefits to caching the inverse rather than computing it again
+## The two functions below try to optimize the solving for the matrix inverse by first checking it in the cache
+ 
+## makeCacheMatrix function creates a special matrix object that can cache its inverse
+ 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the initial value of the matrix inverse to NULL
+  matrixinverse <- NULL                    
+  set <- function(y) {                     
+    x <<- y
+    matrixinverse <<- NULL             
+  }
+  ## get the value of the inverse
+  get <- function() x                           
+  #calculate the inverse of a matrix by the solve function
+  setinverse <- function(solve) matrixinverse <<- solve
+  # get the inverse    
+  getinverse <- function() matrixinverse       
+  ## return      
+  list(set = set, get = get,                   
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
-
+ 
+## cacheSolve function computes the inverse of the special matrix returned by the makeCacheMatrix function
+## if the inverse has already been calculated (and the matrix has not changed) than the inverse is retrieved from the cache
+ 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixinverse <- x$getinverse()
+  #get the inverse if exists
+  if(!is.null(matrixinverse)) {                 
+    message("getting the cached inverse of the matrix")
+    return(matrixinverse)
+  }
+  #if there is no cached inverse, we need to calculate it
+  data <- x$get()                              
+  matrixinverse <- solve(data, ...)
+  x$setinverse(matrixinverse)
+  matrixinverse
 }
